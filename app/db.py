3b5aeb5ceef1592846e78fb579a5e8b6996d4bd5
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-08 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, relationship
-
+import datetime
 
 # connection string for postgresql: postgresql+psycopg2://user:password@host:port/dbname[?key=value&key=value...]
 DATABASE_URL = "postgresql+psycopg2://postgres:root@localhost:5432/Production_DB"
