4db57548050859bc3b50f9486338e94fb48ba7da
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-08 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -3,5 +3,6 @@ import uuid
 
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
