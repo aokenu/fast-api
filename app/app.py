e804ca1a24c1ccf21e6111cfd9123d8b756755b3
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-08 14:30:00 +0100

    Updated project file

--- app/app.py
+++ app/app.py
@@ -51,7 +51,7 @@ def get_post(id: int):
 
 # create a post endpoint
 @app.post("/posts")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostCreate:
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys()) + 1] = new_post
     return new_post
