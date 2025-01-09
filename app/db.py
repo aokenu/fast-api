9189764d07819141f82afc6f2bdb64000d9cf29a
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-09 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -29,9 +29,9 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metabase.create_all)
+        await conn.run_sync(DeclarativeBase.metadata.create_all)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
-        yeild session
+        yield session
