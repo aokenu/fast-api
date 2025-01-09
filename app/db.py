55ffef8b8cc334f62138514792e8a8ccc9327de7
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-09 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -25,3 +25,8 @@ class Post(DeclarativeBase):
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(DeclarativeBase.metabase.create_all)
