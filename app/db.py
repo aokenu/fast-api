7b2ba6f7b1c12e0bfb81ba442b4ae5f4023ab09d
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-15 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -17,7 +17,7 @@ class Base(DeclarativeBase):
 
 
 class Post(Base):
-    __table__ = "posts"
+    __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     caption = Column(Text)
