ad2d5730237bf1870ceb089be70d4cb7ac8c6423
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-20 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -32,9 +32,9 @@ engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+async def create_db_and_tables(): # running this function does not block any other task
+    async with engine.begin() as conn: #
+        await conn.run_sync(Base.metadata.create_all) # while waiting for the database table to be created let me go and do other task
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
