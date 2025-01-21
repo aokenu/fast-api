133fb49b0f5081fe9519b7fd461ef206e4a1e08d
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-21 14:30:00 +0100

    Updated project file

--- app/app.py
+++ app/app.py
@@ -44,22 +44,22 @@ async def upload_file(
 @app.get("/feed")
 async def get_feed(
     session: AsyncSession = Depends(get_async_session)
-):
+): 
     result = await session.execute(select(Post).order_by(Post.created_at.desc()))
     posts = [row[0] for row in result.all()]
 
 
-    posts_data = []
-    for post in posts:
-        posts_data.append(
-            {
-                "id": str(post.id),
-                "caption": post.caption,
-                "url": post.url,
-                "file_type": post.file_type,
-                "file_name": post.file_name,
-                "created_at": post.created_at.isoformat()
-            }
-        )
+    # posts_data = []
+    # for post in posts:
+    #     posts_data.append(
+    #         {
+    #             "id": str(post.id),
+    #             "caption": post.caption,
+    #             "url": post.url,
+    #             "file_type": post.file_type,
+    #             "file_name": post.file_name,
+    #             "created_at": post.created_at.isoformat()
+    #         }
+    #     )
 
-    return {"posts": posts_data}
+    # return {"posts": posts_data}
