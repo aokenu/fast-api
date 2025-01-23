41fe687f30836bb8a45bfe250aa7675229327163
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-23 14:30:00 +0100

    Updated project file

--- app/db.py
+++ app/db.py
@@ -12,31 +12,3 @@ from datetime import datetime
 DATABASE_URL = "postgresql+psycopg://postgres:root@localhost:5432/Production_DB"
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    caption = Column(Text)
-    url = Column(String, nullable=False)
-    file_type = Column(String, nullable=False)
-    file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-
-
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def create_db_and_tables(): 
-    async with engine.begin() as conn: 
-        await conn.run_sync(Base.metadata.create_all) 
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
