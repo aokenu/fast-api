4105d6e4116e82fb0cce422e5a73ef92c5da0968
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-28 14:30:00 +0100

    Updated project file

--- app/images.py
+++ app/images.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 imagekit = ImageKit(
-    private_key=os.getenv("private_vgmbvPo2ZfkpUqFFxc7Xi3/DRdk="),
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
     public_key=os.getenv("public_MhZwgr9l9Q9A25dRxgL+UhxaD5A="),
     url_endpoint=os.getenv("https://ik.imagekit.io/vmucgzqtjs")
 )
