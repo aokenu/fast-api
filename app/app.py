040a51e95bdcc3871e940fa192c8a548ff9b3273
aokenu <34852856+aokenu@users.noreply.github.com>
2025-01-29 14:30:00 +0100

    Updated project file

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
 from app.images import imagekit
- 
+from imagekitio.models.UploadFileRequestOptions importUploadFileRequestOptions
 
 
 
