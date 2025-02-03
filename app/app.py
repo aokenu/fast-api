cecc6df989c960337163c303973c9de0fbb91547
aokenu <34852856+aokenu@users.noreply.github.com>
2025-02-03 14:30:00 +0100

    Updated project file

--- app/app.py
+++ app/app.py
@@ -56,7 +56,9 @@ temp_file_path = None
 try:
     with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splittext(file.filename)[1]) as temp_file:
         temp_file_path = temp_file.name 
-        
+        shutil.copyfileobj(file.file, temp_file)
+
+    
 
 
 ):
